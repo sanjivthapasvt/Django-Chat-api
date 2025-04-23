72a38c47f4335859e8954952e0bd93b49a40056f
sanjivthapa <thapasvt12@gmail.com>
2025-04-23 13:24:43 +0545

    Fixed a small bug for get, put patch method

--- Django_Chat/chat_room/permissions.py
+++ Django_Chat/chat_room/permissions.py
@@ -2,8 +2,12 @@ from rest_framework.permissions import BasePermission
 
 class IsRoomParticipant(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user in obj.participants.all()
-    
+        if hasattr(obj, 'participants'):
+            return request.user in obj.participants.all()
+        elif hasattr(obj, 'room'):
+            #check if user is participant in the message room
+            return request.user in obj.room.participants.all()
+        
 class IsRoomAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user in obj.admins.all()
