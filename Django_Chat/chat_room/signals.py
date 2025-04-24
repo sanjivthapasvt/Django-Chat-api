8badec80117398a1ef4b297c8c5c22c0b85a2de6
sanjivthapa <thapasvt12@gmail.com>
2025-04-24 15:28:57 +0545

    Added notification signal

--- Django_Chat/chat_room/signals.py
+++ Django_Chat/chat_room/signals.py
@@ -1,6 +1,8 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
-from .models import ChatRoom
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from .models import ChatRoom, Message, Notification
 
 
 @receiver(m2m_changed, sender=ChatRoom.participants.through)
@@ -30,3 +32,41 @@ def update_group_room_name(sender, instance, action, pk_set, **kwargs):
         except Exception as e:
             # debbuging
             print(f"Error updating group room name for room {instance.id}: {e}")
+
+
+#Signals for notficatoin
+@receiver(post_save, sender=Message)
+def create_message_notification(sender, instance, created, **kwargs):
+    if not created:
+        return
+    
+    chatroom = instance.room
+    participants = chatroom.participants.all()
+    
+    for user in participants:#skip creating notification for sender
+        if user == instance.sender:
+            continue
+        
+        #create notification
+        notification = Notification.objects.create(
+            user=user,
+            messsage=instance,
+            notification_type = "new_message"
+        )
+        
+        #send realtime notification via websocket
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            f"notificatoin_{user.id}",
+            {
+                "type": "send_notification",
+                "message_id": instance.id,
+                "sender": instance.sender.username,
+                "room_id": chatroom.id,
+                "content": instance.content[:50] + '...' if len(instance.content) >50 else instance.content,
+                "timestamp": notification.timestamp.isoformat(),
+                "is_read": False,
+                "notification_type": notification.notification_type
+         
+            }
+        )
