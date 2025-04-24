ff6302eaf76f51eaf9195adb1fffc25e2404a0b8
sanjivthapa <thapasvt12@gmail.com>
2025-04-24 15:28:21 +0545

    updated Notification model

--- Django_Chat/chat_room/models.py
+++ Django_Chat/chat_room/models.py
@@ -154,10 +154,10 @@ class MessageReadStatus(models.Model):
 class Notification(models.Model):
     """Notification model for new messages"""
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
-    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
-    
+    notification_type = models.CharField(max_length=50, default="new_message")
     class Meta:
         indexes = [
             models.Index(fields=['user', 'is_read']),
