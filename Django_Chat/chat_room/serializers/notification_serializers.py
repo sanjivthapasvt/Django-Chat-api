9ca00d2e561401c1fab93fb93e33b755ca644ac5
sanjivthapa <thapasvt12@gmail.com>
2025-04-25 10:25:12 +0545

    fixed small bug by adding get_message

--- Django_Chat/chat_room/serializers/notification_serializers.py
+++ Django_Chat/chat_room/serializers/notification_serializers.py
@@ -4,6 +4,7 @@ from ..models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
     sender = serializers.SerializerMethodField()
     room_name = serializers.SerializerMethodField()
+    message = serializers.SerializerMethodField()
     class Meta:
         model = Notification
         fields = ['id', 'sender', 'message','room_name', 'timestamp', 'is_read', 'timestamp']
@@ -12,4 +13,7 @@ class NotificationSerializer(serializers.ModelSerializer):
         return obj.message.sender.username
     
     def get_room_name(self, obj) -> str:
-        return obj.message.room.name
+        return obj.message.room.room_name
+    
+    def get_message(self, obj) -> str:
+        return obj.message.content
