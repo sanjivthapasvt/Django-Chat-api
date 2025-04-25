2b8db76a44128723d9c8006552107ff908819bb2
sanjivthapa <thapasvt12@gmail.com>
2025-04-25 10:57:02 +0545

    Added online status to determine if user is online

--- Django_Chat/user_api/models.py
+++ Django_Chat/user_api/models.py
@@ -6,6 +6,7 @@ def user_profile_pic_path(instance, filename):
 
 class User(AbstractUser):
     profile_pic = models.ImageField(upload_to=user_profile_pic_path, blank=True, null=True)
+    online_status = models.BooleanField(default=False, db_index=True)
     REQUIRED_FIELDS = ["email", "first_name", "last_name"]
     USERNAME_FIELD = "username"
     
