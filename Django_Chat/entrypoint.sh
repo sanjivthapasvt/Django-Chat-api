cebe150d4c88fc14bfa7ffd02a78a24ff3095b78
sanjivthapa <thapasvt12@gmail.com>
2025-05-11 15:38:06 +0545

    Cooking

--- Django_Chat/entrypoint.sh
+++ Django_Chat/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py makemigrations chat_room user_api
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+uvicorn Django_Chat.asgi:application --host 0.0.0.0 --port 8000
